6c7e1a51fd38af8cc6f87cd81860a15576cab1ee
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-14 23:14:00 +0000

    feat: add login method to Bank class

--- bank.py
+++ bank.py
@@ -3,6 +3,7 @@ from account import Account
 from utilities import generate_user_id, generate_account_number
 import sqlite3
 from database import create_connection
+from database import get_password_by_user_id
 
 class Bank:
     def __init__(self):
@@ -33,3 +34,15 @@ class Bank:
             except Exception as e:
                  raise e
             return Account(account_number, account_type)
+    
+    def login_user(self, user_id, password):
+         stored_password = get_password_by_user_id(self.conn, user_id)
+         if stored_password is None:
+              print("User ID does not exist.")
+              return False
+         elif stored_password == password:
+              print("Login successful.")
+              return True
+         else:
+              print("Incorrect password.")
+              return False
