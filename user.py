a6e8f7331db81637dc5d8c94766df2b5f4c7f45e
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-27 15:30:13 +0000

    docs: add docstrings

--- user.py
+++ user.py
@@ -1,5 +1,26 @@
 class User:
+    """
+    Represents a user in the banking system.
+
+    Attributes:
+        forename (str): The forename of the user.
+        surname (str): The surname of the user.
+        user_id (str): The unique identifier for the user.
+        password (str): The password for the user.
+        accounts (dict): A dictionary of accounts associated with the user, 
+                         where keys are account numbers and values are Account objects.
+    """
+
     def __init__(self, forename, surname, user_id, password):
+        """
+        Initializes a new User instance with the given details.
+
+        Parameters:
+            forename (str): The forename of the user.
+            surname (str): The surname of the user.
+            user_id (str): The unique identifier for the user.
+            password (str): The password for the user.
+        """
         self.forename = forename
         self.surname = surname
         self.user_id = user_id
@@ -7,9 +28,22 @@ class User:
         self.accounts = {}
 
     def add_account(self, account):
-        # Add an account to the user
+        """
+        Adds an account to the user's list of accounts.
+
+        Parameters:
+            account (Account): The account object to be added to the user's accounts.
+        """
         self.accounts[account.account_number] = account
 
     def get_account(self, account_number):
-        # Retrieve a specific account
+        """
+        Retrieves a specific account associated with the user by account number.
+
+        Parameters:
+            account_number (str): The account number of the account to retrieve.
+
+        Returns:
+            Account: The account object associated with the given account number, if found; otherwise, None.
+        """
         return self.accounts.get(account_number)
